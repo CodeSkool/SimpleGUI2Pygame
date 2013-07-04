c4430ea4c204c0cf5c81a9a2a4c9e88d90f98709
A Sadavoy <not-public@yopmail.com>
2013-07-04 00:46:47 -0400

    Added template files for Sunday Jul 07.

---
+++ Videos/2013Jul07Sun/filehelper.py
@@ -0,0 +1,43 @@
+import pickle
+
+class FileHelper:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def open_file(self, file_name, mode):
+        """Tries to open a file in the specified mode.
+        If the file does not exist, opens it in a+ mode"""
+        try:
+            the_file = open(file_name, mode)
+        except IOError:
+            the_file = open(file_name, "a+")
+        return the_file
+
+    def save(self, object_to_save):
+        """Pickle the object to the file"""
+        file = open(self.filename, "w")
+        try:
+            pickle.dump(object_to_save, file)
+        finally:
+            file.close()
+
+    def load(self):
+        """Unpickle the object from the file if it exists"""
+        loaded_object = None
+        file = self.open_file(self.filename, "r")
+        if file.read(1) <> '':
+           file.close()
+           file = open(self.filename, "r")
+           loaded_object = pickle.load(file)
+        file.close()
+        return loaded_object
+
+def main():
+    o = [1, 2, 3, {4:(5, 6, 7)}, "eight"]
+    helper = FileHelper("test.pkl")
+    helper.save(o)
+    o2 = helper.load()
+    assert o == o2, "invalid pickling"
+
+if __name__ == '__main__':
+    main()
---
+++ Videos/2013Jul07Sun/ui.py
@@ -0,0 +1,159 @@
+import pygame, sys, pgxtra
+
+class UI:
+    def __init__(self, target, title="", width=640,
+                 height=480, font=20,
+                 bg_color="black",
+                 fg_color="white"):
+        """(UI, object, str, int, int, int, str, str) -> NoneType
+
+        Initialize a UI object, and pygame."""
+        self.target = target
+        self.title = title
+        self.width = width
+        self.height = height
+        self.font = font
+        self.bg_color = bg_color
+        self.fg_color = fg_color
+        self.controls = []
+
+        pygame.init()
+        self.clock = self.fpsClock = pygame.time.Clock()
+
+        self.surface = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption(self.title)
+        self.screen = pygame.Surface((self.width, self.height))
+
+    def start(self):
+        """(UI) -> NoneType
+
+        Set up the target and run the main game loop
+        (handling events, drawing)."""
+        # set up the target
+        self.target.setup(self.screen)
+
+        while True:
+            # fill the background color
+            self.screen.fill(pygame.Color(self.bg_color))
+            # handle events
+            self.handle_events()
+            # draw things
+            self.draw()
+
+            # flip the display
+            pygame.display.flip()
+            # update the clock
+            self.fpsClock.tick(30)
+
+
+    def handle_events(self):
+        """(UI) -> NoneType
+
+        Check for events and allow them to be handled."""
+        for event in pygame.event.get():
+            # First, allow the target to handle the event
+            self.target.handle(event)
+            if event.type == pygame.QUIT:
+                # handle the quit event
+                self.quit()
+            else:
+                # allow controls to handle the event
+                for control in self.controls:
+                    control.check_event(event)
+
+    def draw(self):
+        """(UI) -> NoneType
+
+        Allow everything to draw itself."""
+        for control in self.controls:
+            control.draw()
+        self.target.update(self.screen)
+        self.surface.blit(self.screen, (0,0))
+
+    def quit(self):
+        """(UI) -> NoneType
+
+        Notify the target that we are quiting, then quit."""
+        try:
+            self.target.quit()
+        finally:
+            # always quit, even if there are exceptions
+            quit()
+
+    def draw_text(self, screen, text, location):
+        """(UI, Surface, str, (int, int)) -> Surface
+        Draw the specified text to the screen at the specified location.
+        Return the text surface.
+        """
+        if pygame.font:
+            font = pygame.font.Font(None, self.font)
+            text = font.render(text, 1, pygame.Color(self.fg_color))
+            textpos = text.get_rect()
+            textpos.centerx = location[0]
+            textpos.centery = location[1]
+            screen.blit(text, textpos)
+            return text
+
+    def add_label(self, screen, text, location=None):
+        """(UI, surface, str, (int, int)) -> Surface
+        Draw a label (same as draw_text).
+        """
+        return self.draw_text(screen, text, location)
+
+    def add_input(self, screen, text, handler, location=None, size=None):
+        """Add a new InputField to the UI."""
+        if location == None:
+            location = (50, 50)
+        if size == None:
+            size = (200, 50)
+        textinput = pgxtra.InputField(screen, text, self.bg_color, self.fg_color, location, size, handler, self.font)
+        textinput.enabled = True
+        self.controls.append(textinput)
+        return textinput
+
+
+    def add_button(self, screen, text, handler, location=None, size=None):
+        """Add a new Button to the UI."""
+        if location == None:
+            location = (50, 50)
+        if size == None:
+            size = (50, 50)
+        button = pgxtra.Button(screen, text, self.bg_color, self.fg_color, location, size, handler, self.font)
+        button.enabled = True
+        self.controls.append(button)
+        return button
+
+def quit():
+    """Stop Pygame and exit the program."""
+    pygame.quit()
+    sys.exit()
+
+
+def main():
+    class tester:
+        def __init__(self):
+            self.ui = UI(self, "Test Game")
+        def update(self, screen):
+            pass
+        def setup(self, screen):
+            self.ui.add_button(screen, "New button", self.button_handler)
+        def handle(self, event):
+            pass
+        def button_handler(self, btn):
+            pass
+        def start(self):
+            self.ui.start()
+    try:
+        tester().start()
+        import time
+        time.sleep(30)
+    except Exception as e:
+        print e
+    finally:
+        quit()
+
+if __name__ == '__main__':
+   import doctest
+   results = doctest.testmod()
+   if results[0] == 0:
+      main()
