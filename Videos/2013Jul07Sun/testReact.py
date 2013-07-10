1c052e22814810f1ca153dc28ac983886aac8164
JulieAnn <j.ann.w@hotmail.com>
2013-07-09 22:51:43 -0500

    Added unique start position. Collision between shapes still not right.

--- Videos/2013Jul07Sun/testReact.py
+++ Videos/2013Jul07Sun/testReact.py
@@ -31,28 +31,58 @@ color = ['red','green','blue','yellow','ltblue','purple','lime','violet','pink']
 # Classes
 class Square(pygame.sprite.Sprite):
 
-    def __init__(self, surface, size, color):
+    def __init__(self, surface, size, color, shapes):
         pygame.sprite.Sprite.__init__(self)
         self.surface = surface
         self.size = size
         self.color = color
         self.clickable = True
 
-        # Get random start position and velocity
+        # Create surface and fill
+        self.image = pygame.Surface((self.size, self.size))
+        self.image.fill(self.color)
+
+        # Get unique random position and random velocity
+        self.rand_pos(self.size, shapes)
+        self.rand_vel()
+        self.last_vel = [0,0]
+
+        # Get rect for surface and update with position
+        self.rect = self.image.get_rect()
+        self.rect.center = self.get_pos()
+
+        # Draw square
+        self.square = pygame.draw.rect(self.image, self.color, self.rect)
+
+    def rand_pos(self, size, shapes):
+        '''Selects random start position.'''
         xpos = random.randrange(size, W - size)
         ypos = random.randrange(size, H - size)
-        self.pos = [xpos, ypos]
+        pos = [xpos, ypos]
+        rect = pygame.Rect(0,0,size, size)
+        rect.center = pos
+        unique = self.is_unique(rect, shapes)
+        if not unique:
+            self.rand_pos(size, shapes)
+        else:
+            self.pos = pos
+            return pos
 
-        xvel = random.randrange(-5, 5)
-        yvel = random.randrange(-5, 5)
+    def rand_vel(self):
+        '''Selects random start velocity.'''
+        xvel = random.choice([-3,-2,-1,1,2,3])
+        yvel = random.choice([-3,-2,-1,1,2,3])
         self.vel = [xvel, yvel]
+        return self.vel
 
-        # Create surface and draw square
-        self.image = pygame.Surface((size, size))
-        self.image.fill(color)
-        self.rect = self.image.get_rect()
-        self.rect.center = self.pos
-        self.square = pygame.draw.rect(self.image, self.color, self.rect)
+    def is_unique(self, rect, shapes):
+        '''Verifies start position is unique.'''
+        if shapes.sprites() == None:
+            return True
+        for shape in shapes:
+            if rect.colliderect(shape):
+                return False
+        return True
 
     def draw(self):
         self.surface.blit(self.image, self.pos)
@@ -60,9 +90,25 @@ class Square(pygame.sprite.Sprite):
     def update(self, hitlist):
         # If collide with other object, switch velocities
         if self in hitlist:
-            other_vel = hitlist[self][0].get_vel()
-            self.vel, other_vel = other_vel, self.vel
-            hitlist[self][0].change_vel(other_vel)
+            for other in hitlist[self]:
+                other_vel = other.get_vel()
+
+                # Check that both objects have different velocities
+                if self.vel != other_vel:
+                    next_vel, other_vel = other_vel, self.vel
+
+                # If the objects are the same velocity, make both have new
+                # velocities
+                else:
+                    next_vel = self.rand_vel()
+                    other_vel = self.rand_vel()
+
+                # If the next_vel isn't the last vel, update
+                if next_vel != self.last_vel:
+                    self.vel, self.last_vel = next_vel, self.vel
+
+                # Update the other objects velocity
+                other.change_vel(other_vel)
 
         # Update position
         self.pos[0] += self.vel[0]
@@ -71,16 +117,16 @@ class Square(pygame.sprite.Sprite):
         # If near edges, reverse direction
         if self.pos[0] < 0:
             self.pos[0] = 0
-            self.vel[0] = -self.vel[0]
+            self.vel[0] *= -1
         elif self.pos[0] > W - self.size:
             self.pos[0] = W - self.size
-            self.vel[0] = -self.vel[0]
+            self.vel[0] *= -1
         if self.pos[1] < 0:
             self.pos[1] = 0
-            self.vel[1] = -self.vel[1]
+            self.vel[1] *= -1
         elif self.pos[1] > H - self.size:
             self.pos[1] = H - self.size
-            self.vel[1] = -self.vel[1]
+            self.vel[1] *= -1
 
         # Update self.rect position
         self.rect.topleft = self.pos
@@ -97,10 +143,13 @@ class Square(pygame.sprite.Sprite):
     def change_vel(self, new_vel):
         self.vel = new_vel
 
+    def get_pos(self):
+        return self.pos
+
 
 class Circle(pygame.sprite.Sprite):
 
-    def __init__(self, surface, size, color):
+    def __init__(self, surface, size, color, shapes):
         pygame.sprite.Sprite.__init__(self)
         self.surface = surface
         self.radius = size / 2
@@ -131,12 +180,6 @@ class Circle(pygame.sprite.Sprite):
         self.surface.blit(self.image, self.pos)
 
     def update(self, hitlist):
-        # If collide with other object, switch velocities
-        if self in hitlist:
-            other_vel = hitlist[self][0].get_vel()
-            self.vel, other_vel = other_vel, self.vel
-            hitlist[self][0].change_vel(other_vel)
-
         # Update position
         self.pos[0] += self.vel[0]
         self.pos[1] += self.vel[1]
@@ -154,6 +197,12 @@ class Circle(pygame.sprite.Sprite):
         elif self.pos[1] > H - self.size:
             self.pos[1] = H - self.size
             self.vel[1] = -self.vel[1]
+
+        # If collide with other object, switch velocities
+        if self in hitlist:
+            other_vel = hitlist[self][0].get_vel()
+            self.vel, other_vel = other_vel, self.vel
+            hitlist[self][0].change_vel(other_vel)
 
         # Update self.rect position
         self.rect.topleft = self.pos
@@ -182,10 +231,12 @@ def main():
 
     shapes = pygame.sprite.Group()
 
-    for i in range(len(color)):
-        obj1 = Square(surface, 35, COLORS[color[i]])
-        obj2 = Circle(surface, 35, COLORS[color[i]])
-        shapes.add(obj1)
+    while len(shapes) < 30:
+        for i in range(len(color)):
+            obj1 = Square(surface, 35, COLORS[color[i]], shapes)
+
+            #obj2 = Circle(surface, 35, COLORS[color[i]], shapes)
+            shapes.add(obj1)
 
     while True:
         clock.tick(30)
