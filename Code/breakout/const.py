32f30ffa687c28aa30ab6ab40c273902a0c10a9b
JulieAnn <j.ann.w@hotmail.com>
2013-07-22 07:19:04 -0500

    Made PEP8 compliant

--- Code/breakout/const.py
+++ Code/breakout/const.py
@@ -6,21 +6,22 @@ from utilities_1 import ui
 
 # CONSTANTS
 
-W, H = 800, 700 ## Screen width and height
+W, H = 800, 700  # Screen width and height
 
 LEVELS = LL.LevelLoader("resources\\levels.txt").open_file()
 
-DK_PURPLE = pygame.Color(128,  0,255,255)
-BLACK  = pygame.Color(  0,  0,  0,255)
+DK_PURPLE = pygame.Color(128, 0, 255, 255)
+BLACK = pygame.Color(0, 0, 0, 255)
 
 Jules_UIContext = ui.UIContext("Breakout Revisited", W, H, 0,
-                            "resources\\Comfortaa-Regular.ttf", 30,
-                            BLACK, DK_PURPLE, (0,0), (W/10, H/10), 0, 0, 0)
+                               "resources\\Comfortaa-Regular.ttf", 30,
+                               BLACK, DK_PURPLE, (0, 0), (W / 10, H / 10),
+                               0, 0, 0)
 
-REBOUND_SOUND_FILES = { "boop1" : "resources\\boop1.ogg",
-                "beep1" : "resources\\beep1.ogg",
-                "bop1" : "resources\\bop1.ogg",
-                }
+REBOUND_SOUND_FILES = {"boop1": "resources\\boop1.ogg",
+                       "beep1": "resources\\beep1.ogg",
+                       "bop1": "resources\\bop1.ogg"}
+
 
 def main():
     print (W, H)
