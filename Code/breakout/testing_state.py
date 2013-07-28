b1e6a4dc257a3de4a4dc596cf83f04b1ccc461a0
JulieAnn <j.ann.w@hotmail.com>
2013-07-28 12:54:51 -0500

    Fix animation

--- Code/breakout/testing_state.py
+++ Code/breakout/testing_state.py
@@ -1,13 +1,34 @@
 #!/usr/local/bin/python
 
-from utilities_1 import state
+import pygame
+from utilities_1 import state, basesprite
 
 
 class TestingState(state.State):
     def __init__(self):
         state.State.__init__(self)
         pass
+    
+    def setup(self):
+        filename = "resources\\breakout_block_fading.png"
+        width, height, columns = 64, 32, 30
+        sprite = basesprite.BaseSprite(filename, width, height, columns)
+        self.sprite_group = pygame.sprite.Group()
+        self.sprite_group.add(sprite)
+    
+    def update(self, screen):
+        self.sprite_group.update()
+        self.sprite_group.draw(screen)
+
 
 
 if __name__ == "__main__":
-    TestingState().start()
+    import doctest
+    failed = 0
+    result = doctest.testmod()
+    print result
+    if result[failed]:
+        pass
+    else:
+        #pass
+        TestingState().start()
--- Code/breakout/utilities_1/imageloader.py
+++ Code/breakout/utilities_1/imageloader.py
@@ -5,13 +5,16 @@ class ImageLoader:
     def __init__(self, filename):
         try:
             self.image = pygame.image.load(filename)
+            self.rect = self.image.get_rect()
         except pygame.error, message:
             print 'Unable to load image:', filename
             #print message
             raise SystemExit
 
     # Load one sub-image from image given position as Rect object
-    def load(self, rect):
+    def load(self, rect=None):
+        if rect == None:
+            rect = self.rect
         image = pygame.Surface(rect.size)
         image.blit(self.image, (0, 0), rect)
         return image
@@ -23,7 +26,7 @@ class ImageLoader:
     # Load a strip of images given position of first image as Rect object
     # (Note: The images have to be the same size.)
     def load_strip(self, rect, image_count):
-        tuples = [(rect[0]+rect[2]*x, rect[1], rect[2], rect[3])
+        tuples = [(rect[0] + rect[2] * x, rect[1], rect[2], rect[3])
                   for x in range(image_count)]
         return self.load_list(tuples)
 
