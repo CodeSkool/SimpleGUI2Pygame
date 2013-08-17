d657450d002d9d38c6ed1a929bb73f417e9c9d1f
JulieAnn <j.ann.w@hotmail.com>
2013-08-17 11:48:19 -0500

    camera update

--- Code/breakout/utilities_1/camera.py
+++ Code/breakout/utilities_1/camera.py
@@ -4,6 +4,61 @@
 
 
 class Camera(object):
+    
+    # X property
+    def _getx(self):
+        return self.rect.x
+    
+    def _setx(self, value):
+        self.rect.x = value
+        
+    X = property(_getx, _setx)
+    
+    # Y property
+    def _gety(self):
+        return self.rect.y
+    
+    def _sety(self, value):
+        self.rect.y = value
+        
+    Y = property(_gety, _sety)
+    
+    # topleft position property
+    def _getpos(self):
+        return self.rect.topleft
+    
+    def _setpos(self, pos):
+        self.rect.topleft = pos
+        
+    topleft = property(_getpos, _setpos)
+    
+    # center position property
+    def _getcenter(self):
+        return self.rect.center
+    
+    def _setcenter(self, pos):
+        self.rect.center = pos
+        
+    center = property(_getcenter, _setcenter)
+    
+    # width property
+    def _getwidth(self):
+        return self.rect.width
+    
+    def _setwidth(self, value):
+        self.rect.width = value
+        
+    width = property(_getwidth, _setwidth)
+    
+    # height property
+    def _getheight(self):
+        return self.rect.height
+    
+    def _setwidth(self, value):
+        self.rect.height = value
+        
+    height = property(_getheight, _setheight)
+    
     def update(self, source, target):
         pass
 
